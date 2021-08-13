58c012abff1762d934df01f3e32d9daa5f1dc047
Darren <dardu@deloitte.com.cn>
2021-08-13 12:49:15 +0800

    Shop

---
+++ Podfile
@@ -0,0 +1,16 @@
+platform :ios, '12.4'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'Shop' do
+
+pod 'SnapKit'
+pod 'MJRefresh'
+pod 'Moya'
+pod 'Alamofire'
+pod 'HandyJSON'
+
+pod 'PayoneerCheckout'
+
+end
---
+++ Shop/Like/RYJLikeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RYJLikeViewController.swift
+//  Shop
+//
+//  Created by 任一杰 on 2020/12/15.
+//
+
+import UIKit
+
+class RYJLikeViewController: RYJBaseViewController {
+
+    var weatherData: weather? = nil
+    var lifeData: life? = nil
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.titleLabel.text = "Like"
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        getWeatherData()
+        getLifeData()
+    }
+    
+    func getWeatherData() {
+        RYJShopProvider.request(.weather) { [self] result in
+            if case let .success(response) = result {
+                // 解析数据
+                let jsonString = String(data: response.data, encoding: .utf8)
+                self.weatherData = weather.deserialize(from: jsonString, designatedPath: "result")
+            }
+        }
+    }
+    
+    func getLifeData() {
+        RYJShopProvider.request(.life) { [self] result in
+            if case let .success(response) = result {
+                // 解析数据
+                let jsonString = String(data: response.data, encoding: .utf8)
+                self.lifeData = life.deserialize(from: jsonString, designatedPath: "result.life")
+            }
+        }
+    }
+}
